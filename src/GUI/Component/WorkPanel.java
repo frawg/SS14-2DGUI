1a0de7d9f1cb80092defb347f47efab4391036d7
frawg <joelpangsg@outlook.com>
2014-07-15 17:55:27 +0800

    Added flag for differentiating between tools

--- src/GUI/Component/WorkPanel.java
+++ src/GUI/Component/WorkPanel.java
@@ -18,21 +18,26 @@ import GUI.Component.Labels.JLabel;
 import javax.swing.JPanel;
 
 public class WorkPanel extends JPanel implements MouseListener, MouseMotionListener {
+	public enum SelectedType
+	{
+		NOTHING, LINE, DEVICE
+	};
 	private ArrayList<JLabel> items = null;
 	private JLabel selected, mouseOver = null;
 	private int _dragFromX, _dragFromY, _initX, _initY;
-	private boolean newItem;
+	private SelectedType seltype;
 	
 	public WorkPanel()
 	{
+		seltype = SelectedType.NOTHING;
 		items = new ArrayList<JLabel>();
-		newItem = false;
 		this.setLayout(null);
 		addMouseListener(this);
 		addMouseMotionListener(this);
 	}
 
-	public void setSelected(JLabel temp) { this.selected = temp; newItem = true; System.out.println(temp.getText() + " clicked"); System.out.println(selected.getPreferredSize()); }
+	public void setSelected(JLabel temp) { this.selected = temp; this.seltype = SelectedType.DEVICE; System.out.println(temp.getText() + " clicked"); System.out.println(selected.getPreferredSize()); }
+	public void setLine(){ this.selected = null; this.seltype = SelectedType.LINE; }
 	
 	@Override
 	public void mouseClicked(MouseEvent e) {
@@ -44,7 +49,7 @@ public class WorkPanel extends JPanel implements MouseListener, MouseMotionListe
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		System.out.println("Mouse entered.");
-		if (selected != null && newItem && selected.getParent() != this)
+		if (selected != null && seltype == SelectedType.DEVICE && selected.getParent() != this)
 		{
 			//selected.setForeground(new Color(0, 0, 0, .5f));
 			selected.setSolidity(0.5f);
@@ -63,75 +68,74 @@ public class WorkPanel extends JPanel implements MouseListener, MouseMotionListe
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
 		System.out.println("Mouse exit.");
-		if (newItem)
+		if (seltype == SelectedType.DEVICE)
 		{
 			remove(selected);
 		}
 
-		newItem = false;
+		seltype = SelectedType.NOTHING;
 		selected = null;
 		
 		this.repaint();
 	}
 	
 	@Override
-	public void mousePressed(MouseEvent e) { 
-		if (SwingUtilities.isRightMouseButton(e))
-		{
-			for (JLabel j : items)
-				if (j.contains(e.getPoint()))
-				{
-					System.out.println(j.getText() + " right click.");
-					break;
-				}
-		}
-		
+	public void mousePressed(MouseEvent e)
+	{
 		System.out.println(getParent().getWidth() + ", " + getParent().getHeight() + "\t\t" + e.getX() + ", " + e.getY());
-		int x = e.getX();   // Save the x coord of the click
-		int y = e.getY();   // Save the y coord of the click
-		System.out.print(x + ", " + y);
-		//... Find card image this is in.  Check from top down.
+		System.out.print(e.getX() + ", " + e.getY());
 		
-		if (!newItem) // not new item
+		if (seltype == SelectedType.NOTHING) // not new item
 		{
 			System.out.print("\tNot new item");
 			if (selected == null)  // Assume not in any image.
 			{
-				System.out.println("\tSelected is null.");
-				for (int crd=items.size()-1; crd>=0; crd--)
-//				for (int crd = 0; crd < items.size(); crd++)
-				{
-					JLabel testCard = items.get(crd);
-//					if (compareLocation(testCard, x, y)) {
-					if (testCard.contains(x, y)) {
-						//... Found, remember this card for dragging.
-						_dragFromX = x - testCard.getX();  // how far from left
-						_dragFromY = y - testCard.getY();  // how far from top
-						selected = testCard;  // Remember what we're dragging.
-						break;        // Stop when we find the first match.
-					}	
-				}
+				selectItem(e);
 			}
 		}
-		else
+		else if (seltype == SelectedType.DEVICE)
 		{
-			System.out.println("\tNew item: " + selected.getText() + ".");
-//			selected.setBounds(e.getX() - (32), e.getY() - (32), (int)selected.getPreferredSize().getWidth(), (int)selected.getPreferredSize().getHeight());
-			selected.setLocation(e.getX() - (32), e.getY() - (32));
-			selected.setSolidity(1.0f);
-			_dragFromX = 32;
-			_dragFromY = 32;
-			this.add(selected, 0);
-			items.add(selected);
-			newItem = false;
-			this.repaint();
+			placeNewItem(e);
+		}	
+	}
+	
+	private void selectItem(MouseEvent e)
+	{
+		System.out.println("\tSelected is null.");
+		for (int crd=items.size()-1; crd>=0; crd--) //... Find card image this is in.  Check from top down.
+//		for (int crd = 0; crd < items.size(); crd++)
+		{
+			JLabel testCard = items.get(crd);
+			if (testCard.contains(e.getX(), e.getY())) {
+				//... Found, remember this card for dragging.
+				_dragFromX = e.getX() - testCard.getX();  // how far from left
+				_dragFromY = e.getY() - testCard.getY();  // how far from top
+				selected = testCard;  // Remember what we're dragging.
+					if (SwingUtilities.isRightMouseButton(e))
+						System.out.println(selected.getText() + " right click.");
+				break;        // Stop when we find the first match.
+			}	
 		}
 	}
-
+	
+	private void placeNewItem(MouseEvent e)
+	{
+		System.out.println("\tNew item: " + selected.getText() + ".");
+//		selected.setBounds(e.getX() - (32), e.getY() - (32), (int)selected.getPreferredSize().getWidth(), (int)selected.getPreferredSize().getHeight());
+		selected.setLocation(e.getX() - (32), e.getY() - (32));
+		selected.setSolidity(1.0f);
+		_dragFromX = 32;
+		_dragFromY = 32;
+		this.add(selected, 0);
+		items.add(selected);
+		seltype = SelectedType.NOTHING;
+		this.repaint();
+	}
+	
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		selected = null;
-		newItem = false;
+		seltype = SelectedType.NOTHING;
 	}
 
 	@Override
@@ -178,7 +182,6 @@ public class WorkPanel extends JPanel implements MouseListener, MouseMotionListe
             add(selected, 0);
             items.remove(selected);
             items.add(selected);
-            //selected.setLocation(e.getX(), e.getY());
             
             this.repaint(); // Repaint because position changed.
 		}
